98ea50052b9ecf837d74ca4528423d3f91c82a79
Royce Jiang <jiangxd@embedinfo.com>
2013-12-24 10:12:29 +0800

    SDcard: support sdcard mount for sam9g25 and sama5d3
    The node for the 2 boards are different, we make them separated to
    support both of them
    
    Signed-off-by: Royce Jiang <jiangxd@embedinfo.com>

--- common/config/Android_Copy.mk
+++ common/config/Android_Copy.mk
@@ -5,7 +5,6 @@ LOCAL_PATH := device/atmel/common/config
 # system configuration files
 #
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
 	$(LOCAL_PATH)/asound.conf:system/etc/asound.conf \
 	$(LOCAL_PATH)/android.conf:system/etc/android.conf \
 	$(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
@@ -18,10 +17,12 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml 
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),sam9g25-ek)
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/mixer_paths_sam9g25.xml:system/etc/mixer_paths.xml
+	$(LOCAL_PATH)/mixer_paths_sam9g25.xml:system/etc/mixer_paths.xml \
+	$(LOCAL_PATH)/vold_sam9g25.fstab:system/etc/vold.fstab 
 else
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/mixer_paths.xml:system/etc/mixer_paths.xml
+	$(LOCAL_PATH)/mixer_paths.xml:system/etc/mixer_paths.xml \
+	$(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab 
 endif
 ifeq ($(PREBUILD_FIRMWARE), realtek)
 PRODUCT_COPY_FILES += \
